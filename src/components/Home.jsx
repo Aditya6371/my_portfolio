42300081e0e692274379be8f2977f7d8b4614e3b
aditya <adityadas9439@gmail.com>
2025-07-02 09:44:52 +0530

    home image updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -104,7 +104,7 @@ function Home() {
             <div className="absolute inset-0 bg-gradient-to-r from-[#64ffda]/20 to-transparent rounded-2xl blur-xl group-hover:blur-2xl transition-all duration-500"></div>
 
             {/* Profile image container */}
-            <div className="relative w-56 h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-88 xl:h-88">
+            <div className="relative w-48 h-48 sm:w-56 sm:h-56 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-88 xl:h-88">
               {/* Image frame with gradient border */}
               <div className="relative w-full h-full p-1 bg-gradient-to-br from-[#64ffda] via-[#64ffda]/50 to-transparent rounded-2xl group-hover:from-[#64ffda] group-hover:to-[#64ffda]/70 transition-all duration-500">
                 <div className="w-full h-full rounded-2xl overflow-hidden bg-[#0a192f]">
@@ -117,13 +117,13 @@ function Home() {
               </div>
 
               {/* Floating accent border */}
-              <div className="absolute inset-0 border-2 border-[#64ffda]/30 rounded-2xl translate-x-4 translate-y-4 md:translate-x-6 md:translate-y-6 -z-10 group-hover:translate-x-2 group-hover:translate-y-2 md:group-hover:translate-x-3 md:group-hover:translate-y-3 group-hover:border-[#64ffda]/50 transition-all duration-500"></div>
+              <div className="absolute inset-0 border-2 border-[#64ffda]/30 rounded-2xl translate-x-3 translate-y-3 sm:translate-x-4 sm:translate-y-4 md:translate-x-6 md:translate-y-6 -z-10 group-hover:translate-x-1.5 group-hover:translate-y-1.5 sm:group-hover:translate-x-2 sm:group-hover:translate-y-2 md:group-hover:translate-x-3 md:group-hover:translate-y-3 group-hover:border-[#64ffda]/50 transition-all duration-500"></div>
 
               {/* Corner accents */}
-              <div className="absolute -top-2 -left-2 w-6 h-6 border-l-2 border-t-2 border-[#64ffda] group-hover:w-8 group-hover:h-8 transition-all duration-300"></div>
-              <div className="absolute -top-2 -right-2 w-6 h-6 border-r-2 border-t-2 border-[#64ffda] group-hover:w-8 group-hover:h-8 transition-all duration-300"></div>
-              <div className="absolute -bottom-2 -left-2 w-6 h-6 border-l-2 border-b-2 border-[#64ffda] group-hover:w-8 group-hover:h-8 transition-all duration-300"></div>
-              <div className="absolute -bottom-2 -right-2 w-6 h-6 border-r-2 border-b-2 border-[#64ffda] group-hover:w-8 group-hover:h-8 transition-all duration-300"></div>
+              <div className="absolute -top-1.5 -left-1.5 sm:-top-2 sm:-left-2 w-5 h-5 sm:w-6 sm:h-6 border-l-2 border-t-2 border-[#64ffda] group-hover:w-6 group-hover:h-6 sm:group-hover:w-8 sm:group-hover:h-8 transition-all duration-300"></div>
+              <div className="absolute -top-1.5 -right-1.5 sm:-top-2 sm:-right-2 w-5 h-5 sm:w-6 sm:h-6 border-r-2 border-t-2 border-[#64ffda] group-hover:w-6 group-hover:h-6 sm:group-hover:w-8 sm:group-hover:h-8 transition-all duration-300"></div>
+              <div className="absolute -bottom-1.5 -left-1.5 sm:-bottom-2 sm:-left-2 w-5 h-5 sm:w-6 sm:h-6 border-l-2 border-b-2 border-[#64ffda] group-hover:w-6 group-hover:h-6 sm:group-hover:w-8 sm:group-hover:h-8 transition-all duration-300"></div>
+              <div className="absolute -bottom-1.5 -right-1.5 sm:-bottom-2 sm:-right-2 w-5 h-5 sm:w-6 sm:h-6 border-r-2 border-b-2 border-[#64ffda] group-hover:w-6 group-hover:h-6 sm:group-hover:w-8 sm:group-hover:h-8 transition-all duration-300"></div>
             </div>
           </div>
         </motion.div>
